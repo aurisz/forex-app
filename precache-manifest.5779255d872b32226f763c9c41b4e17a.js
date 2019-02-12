7d91665f732236738063ba9c37805d2d18e7d3d6
Auri <auri.zikrullah@qraved.com>
2019-02-12 22:46:43 +0700

    Updates

---
+++ precache-manifest.5779255d872b32226f763c9c41b4e17a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc4c36edf68c0e948801",
+    "url": "/forex-app/static/js/runtime~main.fc4c36ed.js"
+  },
+  {
+    "revision": "544662a6c84649efd256",
+    "url": "/forex-app/static/js/main.544662a6.chunk.js"
+  },
+  {
+    "revision": "5f378e5678d35bbe5092",
+    "url": "/forex-app/static/js/1.5f378e56.chunk.js"
+  },
+  {
+    "revision": "544662a6c84649efd256",
+    "url": "/forex-app/static/css/main.6cf959ed.chunk.css"
+  },
+  {
+    "revision": "5f378e5678d35bbe5092",
+    "url": "/forex-app/static/css/1.533d4b0a.chunk.css"
+  },
+  {
+    "revision": "5c5650d4be0eb7bf11417cc31f6d9120",
+    "url": "/forex-app/index.html"
+  }
+];
